b5adef83d2ecbcac8d0a7c963786d9f32e26fa81
Boopesh Mahendran <boopeshmahendran@gmail.com>
2017-03-21 23:05:16 +0530

    Added loader in the page when form submitted

--- public/javascript/shorten.js
+++ public/javascript/shorten.js
@@ -1,11 +1,14 @@
 $('#url-form').submit(function(event) {
   event.preventDefault();
+  $('#loader').show();
+  $('#shortened-url').empty();
   $.post({
     url: '/api/shorten',
     data: {
       longUrl: $('#longUrl').val()
     },
     success: function(data) {
+      $('#loader').hide();
       var html = '<a href="' + data.shortUrl + '">' + data.shortUrl + '</a>';
       $('#shortened-url').html('Shortened url is ' + html);
     }
